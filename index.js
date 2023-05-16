8a6a5ecc31a13d3304a83a45b8cd8c14f9ef710a
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-05-16 09:00:33 +0200

    finished exercise

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 // comment for console log
 console.log ('Hello Cheslyn')
-
+let a= 2+2;
 alert('This is the browsers alert box')
+
+let firstName= 'Cheslyn';
+console.log(firstName)
+
+const interestRate= 0.3;
+console.log(interestRate)
+
+const person= {
+    name: 'Timothy',
+    surname: 'Barry',
+    age: 12,
+    occupation: 'idiot'
+};
+
+let {name, surname, age, occupation}= person;
+console.log(name);
+console.log(surname);
+console.log(age); 
+console.log(occupation);
